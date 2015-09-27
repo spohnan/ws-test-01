d0fcb427ad7b54465a8c56ccc4ef0782d963ce6c
Andy Spohn <Andy@AndySpohn.com>
2015-09-27 08:20:21 -0400

    Roll to v1.1.0-dev

--- config.go
+++ config.go
@@ -5,12 +5,12 @@ import (
 )
 
 // The main version number that is being run at the moment.
-const Version = "1.0.1"
+const Version = "1.1.0"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-const VersionPrerelease = ""
+const VersionPrerelease = "dev"
 
 // The git commit that was compiled. This will be filled in by the compiler.
 var GitCommit string
