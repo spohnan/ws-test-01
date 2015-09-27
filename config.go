9c0d83643ff028f0bc6f58c5b8abd2ddab151c99
Andy Spohn <Andy@AndySpohn.com>
2015-09-27 08:03:44 -0400

    Roll v1.0.1 release

--- config.go
+++ config.go
@@ -10,7 +10,7 @@ const Version = "1.0.1"
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-const VersionPrerelease = "SNAPSHOT"
+const VersionPrerelease = ""
 
 // The git commit that was compiled. This will be filled in by the compiler.
 var GitCommit string
